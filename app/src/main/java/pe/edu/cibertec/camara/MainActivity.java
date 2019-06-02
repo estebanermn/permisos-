78f8a22bc60a6b26829e6defc588d01e92ea5e4b
EXTENSION <estebanermn@gmail.com>
2019-06-02 11:23:58 -0500

    agregando i

--- app/src/main/java/pe/edu/cibertec/camara/MainActivity.java
+++ app/src/main/java/pe/edu/cibertec/camara/MainActivity.java
@@ -4,12 +4,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,13 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_CAMERA = 1;
     static final int REQUEST_TAKE_PICTURE = 2;
+    static final String AUTORITY_CAMARA = "pe.edu.cibertec.camara";
 
     //Ruta absoluta de la imagen ;
-    String currentPathImage ;
+    String currentPathImage;
 
     //    cmta2112
 
-    Button btCamara;
+    Button btCamara, btnGallery;
+
     ImageView ivPhoto;
 
     @Override
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btCamara = findViewById(R.id.btCamara);
+        btnGallery = findViewById(R.id.btnGallery);
         ivPhoto = findViewById(R.id.ivPhoto);
 
         btCamara.setOnClickListener(new View.OnClickListener() {
@@ -52,18 +57,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                File file = new File(currentPathImage);
+                Uri contentUri = Uri.fromFile(file);
+                intent.setData(contentUri);
+                MainActivity.this.sendBroadcast(intent);
+            }
+        });
     }
 
     private void takePicture() {
 
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermission();
+        } else {
 
-        //validar que la cámara este disponible
-        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-            //Verificar que se disponga del permiso
-            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermission();
-            } else {
+            //validar que la cámara este disponible
+            if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                //Verificar que se disponga del permiso
 
                 File photoFile = null;
                 try {
@@ -71,7 +87,12 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                startActivityForResult(cameraIntent, REQUEST_TAKE_PICTURE);
+                if (photoFile != null) {
+                    Uri photoUri = FileProvider.getUriForFile(this, AUTORITY_CAMARA, photoFile);
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                    startActivityForResult(cameraIntent, REQUEST_TAKE_PICTURE);
+
+                }
             }
         }
     }
@@ -82,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
 
+        //asignar
         //crear el archivo
         File storgeDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg", storgeDir);
@@ -101,10 +123,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == REQUEST_TAKE_PICTURE && resultCode == RESULT_OK) {
-//            Glide.with(this).load(currentPathImage).into(ivPhoto);
+            Glide.with(this).load(currentPathImage).into(ivPhoto);
 
-            Bitmap  bitmap = (Bitmap) data.getExtras().get("data");
-            ivPhoto.setImageBitmap(bitmap);
+
+//            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+//            ivPhoto.setImageBitmap(bitmap);
         }
     }
 
